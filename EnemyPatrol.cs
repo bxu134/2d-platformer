75cf1d8a175d895aca0f46668cab4b45bcb164c1
bxu134 <33552518+bxu134@users.noreply.github.com>
2019-03-10 12:11:46 -0700

    EnemyPatrol.cs
    enemy patrol ai

--- EnemyPatrol.cs
+++ EnemyPatrol.cs
@@ -36,12 +36,5 @@ public class EnemyPatrol : MonoBehaviour
             }
         }
     }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            Destroy(collision.gameObject);
-        }
-    }
 }
+
